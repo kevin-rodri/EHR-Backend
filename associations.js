02a4b80f728aef44d47e5a0517e64963aea3281c
kevin-rodri <kr399880@gmail.com>
2024-11-12 03:06:00 -0500

    added patient order  association

--- associations.js
+++ associations.js
@@ -6,15 +6,20 @@ Description: File is used to establish associations between models.
 const User = require("./models/User");
 const Section = require("./models/Section");
 const Patient = require("./models/Patient");
+const PatientOrder = require("./models/PatientOrders");
 
 function setupAssociations() {
-  // foreign key associations
+  // 1:N relationship between User and Section
   User.hasMany(Section, { foreignKey: "user_id" });
   Section.belongsTo(User, { foreignKey: "user_id" });
 
-  // Reference associations needed
+  // 1:N relationship between Patient and Section
   Patient.hasMany(Section, { foreignKey: "patient_id" });
   Section.belongsTo(Patient, { foreignKey: "patient_id" });
+
+  // 1:N relationship between Patient and PatientOrder
+  Patient.hasMany(PatientOrder, { foreignKey: "patient_id" });
+  PatientOrder.belongsTo(Patient, { foreignKey: "patient_id" });
 }
 
 module.exports = setupAssociations;
